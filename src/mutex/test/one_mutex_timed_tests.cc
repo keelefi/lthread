2db4761f8cd0d9002094e7c7335c1139dc45d6fb
Michele Lindroos <michele.lindroos@gmail.com>
2015-06-03 20:18:19 +0300

    mutex: implement first test
    Implement a test where two threads try to get the same lock. If
    the locking works, the second thread gets blocked and has to
    wait for the first one.

--- src/mutex/test/one_mutex_timed_tests.cc
+++ src/mutex/test/one_mutex_timed_tests.cc
@@ -2,13 +2,48 @@
 
 #include "gtest/gtest.h"
 
+#include <atomic>
+#include <thread>
+
 class OneMutexTimedTests: public testing::Test
 {
+public:
+    lthread::mutex m_mutex;
+
+    OneMutexTimedTests() :
+            m_mutex()
+    {
+    }
 };
 
 TEST_F(OneMutexTimedTests, two_threads_access_same_mutex_second_blocks)
 {
-    ASSERT_TRUE(true);
+    std::atomic<bool> thread2_got_lock(false);
+
+    std::thread thread1(
+            [this]
+            {
+                m_mutex.lock();
+                std::this_thread::sleep_for(std::chrono::seconds(3));
+                m_mutex.unlock();
+            });
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    std::thread thread2(
+            [this, &thread2_got_lock]
+            {
+                m_mutex.lock();
+                thread2_got_lock.store(true, std::memory_order_relaxed);
+                m_mutex.unlock();
+            });
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    EXPECT_FALSE(thread2_got_lock.load(std::memory_order_relaxed));
+
+    thread1.join();
+    thread2.join();
 }
 
 int main(int argc, char **argv)

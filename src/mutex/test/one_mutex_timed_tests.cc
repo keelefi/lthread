8bedba4e2fa56eccdfae0d1ce63a284788987ad2
Michele Lindroos <michele.lindroos@gmail.com>
2015-06-29 21:46:47 +0300

    mutex: add test where 100 threads update the same variable

--- src/mutex/test/one_mutex_timed_tests.cc
+++ src/mutex/test/one_mutex_timed_tests.cc
@@ -4,6 +4,7 @@
 
 #include <atomic>
 #include <thread>
+#include <vector>
 
 class OneMutexTimedTests: public testing::Test
 {
@@ -60,6 +61,39 @@ TEST_F(OneMutexTimedTests, two_threads_access_same_mutex_second_blocks)
 
     thread1.join();
     thread2.join();
+}
+
+/**
+ * 100 threads try to increment the same variable 10000 times. The variable is
+ * expected to hold 100 * 10000 at the end of the test.
+ */
+TEST_F(OneMutexTimedTests, one_hundred_threads_update_same_variable)
+{
+    int thread_count = 100;
+    int increments_per_thread = 10000;
+    std::vector<std::thread> threads;
+    int variable = 0;
+
+    for (unsigned int i = 0; i < thread_count; ++i)
+    {
+        threads.emplace_back(
+                [this, &increments_per_thread, &variable]
+                {
+                    for (unsigned int i = 0; i < increments_per_thread; ++i)
+                    {
+                        m_mutex.lock();
+                        ++variable;
+                        m_mutex.unlock();
+                    }
+                });
+    }
+
+    for (unsigned int i = 0; i < thread_count; ++i)
+    {
+        threads[i].join();
+    }
+
+    EXPECT_EQ(thread_count * increments_per_thread, variable);
 }
 
 int main(int argc, char **argv)
